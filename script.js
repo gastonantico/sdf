c5f1b1c734ef808c6f4c22ad3aa78136c7ac2d98
gastonantico <gastee@gmail.com>
2022-04-12 20:19:34 -0300

    algo

---
+++ script.js
@@ -0,0 +1,39 @@
+let alumno = "jorge"
+let nota = 9
+
+const entidad = 'gobierno ciudad'
+
+console.log(alumno)
+
+let User = 'juan carlos'
+let usuario
+let $user
+let nombre_apellido = 'snake plisken'
+
+
+console.log(User)
+
+
+let str1 = 'juan'
+let str2 = 'pedro'
+let str3 = 'ramiro'
+
+let str4 = str1 + ' y ' + str2 
+let str5 = `${str1} y ${str2}`
+
+console.log(str5)
+
+
+
+let num1 = parseFloat(prompt('mete el primer numero'))
+let num2 = parseFloat(prompt('mete el segundo numero'))
+
+
+let suma = num1 + num2
+let resta = num1 - num2
+let multi = num1 * num2
+
+
+console.log(suma)
+console.log(resta)
+console.log(multi)
